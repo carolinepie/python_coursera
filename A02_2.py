4bb6e4f4034d8e0b912c731559309d47167d93a1
carolinepie <peizhenning@gmail.com>
2016-06-03 10:29:11 -0400

    Update A02_2.py

--- A02_2.py
+++ A02_2.py
@@ -1,4 +1,17 @@
-hrs = raw_input("input hours please: ")
-rate = raw_input("input rate please: ")
-wage = float(rate)*float(hrs)
-print 'Pay:',wage
+#write a pay computation to give employee 1.5 times
+#hourly rate for overtime, if they have worked above
+# 40 hours, do not consider case of bad inputs
+
+def computepay(hrs, rate):
+    if (hrs > 40):
+        return 40*rate+1.5*(hrs-40)*rate
+    else:
+        return float(rate)*float(hrs)
+        
+hrs_str = raw_input("input hours please: ")
+rate_str = raw_input("input rate please: ")
+
+hrs = float(hrs_str)
+rate = float(rate_str)
+
+print "Pay",computepay(hrs, rate)
